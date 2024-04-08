2f1cca0a5750ff8286cd9e1fc628fa0d93d5ced5
YuLiu-web <liuyu9671@gmail.com>
2024-04-08 14:00:40 +0200

    .

---
+++ lsfm_destripe_jax/bin/run_destripe.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This sample script will get deployed in the bin directory of the
+users' virtualenv when the parent module is installed using pip.
+"""
+
+import argparse
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
