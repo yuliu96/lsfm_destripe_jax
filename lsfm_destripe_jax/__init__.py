e6df431cb86803d0f70ae82fdba5ac16715a5624
YuLiu-web <liuyu9671@gmail.com>
2024-04-08 14:01:01 +0200

    .

---
+++ lsfm_destripe_jax/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Yu Liu"
+__email__ = "liuyu9671@gmail.com"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.1"
+
+
+def get_module_version():
+    return __version__
