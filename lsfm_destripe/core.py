365cb7fabbd16da200a13b03375138dd187676be
Yu Liu <liuyu9671@gmail.com>
2024-06-01 20:09:32 +0200

    Update core.py

--- lsfm_destripe/core.py
+++ lsfm_destripe/core.py
@@ -146,7 +146,7 @@ class DeStripe:
             dualtargetd = resize(
                 dualtarget, (1, 1, md, nd), order = 1, anti_aliasing = False,
             )
-        mask = resize(mask, (1, 1, md, nd), order = 1, anti_aliasing = False,)
+        mask = resize(mask+.0, (1, 1, md, nd), order = 1, anti_aliasing = False,)
         X = jnp.array(X)
         mask = jnp.array(mask)
         dualtarget = jnp.array(dualtarget)
