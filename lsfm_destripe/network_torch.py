5c269dc8854784330fe2d2e3b27530e08216f5f2
Yu Liu <liuyu9671@gmail.com>
2024-12-24 23:23:24 +0100

    Update network_torch.py

--- lsfm_destripe/network_torch.py
+++ lsfm_destripe/network_torch.py
@@ -237,7 +237,7 @@ class non_positive_unit:
     def __call__(self, x, target):
         x_non_pos = torch.clip(x - target, 0, None) + target
         x = torch.where(
-            torch.arange((x.shape[2])[None, None, :, None].to(x.device) % 2) == 1,
+            (torch.arange(x.shape[2])[None, None, :, None].to(x.device) % 2) == 1,
             x,
             x_non_pos,
         )
